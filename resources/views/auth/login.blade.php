bc87025c3f1fb3fa14d08c75d4d78754f4fdac45
Alfin Aswar <alfin7aswar@gmail.com>
2026-06-30 21:44:13 +0700

    test deploy OK

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,7 +97,7 @@
                                             <input class="form-check-input primary" type="checkbox" name="remember"
                                                 id="flexCheckChecked" {{ old('remember') ? 'checked' : '' }}>
                                             <label class="form-check-label text-dark" for="flexCheckChecked">
-                                                Ingat Perangkat Ini. test berhasil
+                                                Ingat Perangkat Ini Test Deploy OK
                                             </label>
                                         </div>
                                         {{-- @if (Route::has('password.request'))
