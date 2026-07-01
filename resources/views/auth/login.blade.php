6bbb61d983b315027d600abcfd46ccfdc4d4a2ef
Alfinaswar <alfin7aswar@gmail.com>
2026-07-01 08:45:10 +0700

    222

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,7 +97,7 @@
                                             <input class="form-check-input primary" type="checkbox" name="remember"
                                                 id="flexCheckChecked" {{ old('remember') ? 'checked' : '' }}>
                                             <label class="form-check-label text-dark" for="flexCheckChecked">
-                                                Ingat Perangkat Ini Test Deploy OK222
+                                                Ingat Perangkat Ini Test Deploy OK222 OK
                                             </label>
                                         </div>
                                         {{-- @if (Route::has('password.request'))
