ecaf70a438fee8955aff48c8ebc0e9b2bf201181
Alfinaswar <alfin7aswar@gmail.com>
2026-07-01 09:23:55 +0700

    OK Test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -97,7 +97,7 @@
                                             <input class="form-check-input primary" type="checkbox" name="remember"
                                                 id="flexCheckChecked" {{ old('remember') ? 'checked' : '' }}>
                                             <label class="form-check-label text-dark" for="flexCheckChecked">
-                                                Ingat Perangkeat Ini Sudah OKK
+                                                Ingat Perangkeat Ini Sudah OKK1
                                             </label>
                                         </div>
                                         {{-- @if (Route::has('password.request'))
